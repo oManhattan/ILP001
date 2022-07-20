502f969fde08ba8ed5253a85978d39b5eb9176ae
oManhattan <matheusbatuque@gmail.com>
2022-07-20 15:20:26 -0300

    Initial Commit

---
+++ ILP001/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ILP001
+//
+//  Created by Matheus Cavalcanti de Arruda on 20/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ILP001/ILP001App.swift
@@ -0,0 +1,17 @@
+//
+//  ILP001App.swift
+//  ILP001
+//
+//  Created by Matheus Cavalcanti de Arruda on 20/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ILP001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
